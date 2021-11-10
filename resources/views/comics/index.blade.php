58328b882c77e3fa9cc46228b0f6676a3ff19849
eliaraguso <elia.raguso@gmail.com>
2021-11-10 19:13:40 +0100

    popolata pagina index(tutte le info dei fumetti)prendendo i dati del database

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,33 @@
+@extends("layout.base")
+
+@section('pageContent')
+    <h1>Lista di tutti i fumetti</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Serie</th>
+                <th scope="col">Data di vendita</th>
+                <th scope="col">Tipo</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comics as $comic)
+            <tr>
+                <th scope="row">{{ $comic["id"] }}</th>
+                <td>{{ $comic["title"] }}</td>
+                <td>{{ $comic["description"] }}</td>
+                <td>{{ $comic["price"] }}</td>
+                <td>{{ $comic["series"] }}</td>
+                <td>{{ $comic["sale_date"] }}</td>
+                <td>{{ $comic["type"] }}</td>
+                <td><button type="button" class="btn btn-primary">Visualizza</button></td>
+
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
